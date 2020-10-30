74ec6f719c8145a70809f4dc0bc86e6e2eb678d7
sjahn <asj214@naver.com>
2020-10-31 07:24:20 +0900

    card list add

--- lib/main/main_card.dart
+++ lib/main/main_card.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 var cardLists = [
   {
     'id': 1,
+    'url': 'https://2.bp.blogspot.com/-6u4LfrCn1W0/W6RGi5O2DlI/AAAAAAACVzE/aiOYKSX4ISQD5T2r0WxZAnGI6w_oybJNwCLcBGAs/s1600/%25EC%2595%2584%25EC%259D%25B4%25EC%259C%25A0%2B186.JPG',
+    'title': '제일 좋아하는 아이유 사진'
+  },
+    {
+    'id': 2,
     'url': 'https://1.bp.blogspot.com/-Zkws5VpzVdU/W6RGm9DqK5I/AAAAAAACVzs/04DOhteKaukITiIVUB9UwuCdHsPEyzFLQCLcBGAs/s1600/%25EC%2595%2584%25EC%259D%25B4%25EC%259C%25A0%2B191.jpg',
     'title': '참이슬 같은 아이유'
   },
   {
-    'id': 2,
-    'url': 'https://4.bp.blogspot.com/-jT9IaXg0d_I/W6RFHGbN4LI/AAAAAAACVmc/zDZ_5PwYymAEgxMIHkI1_--6J17w8JGrACLcBGAs/s1600/%25EC%2595%2584%25EC%259D%25B4%25EC%259C%25A0%2B103.jpg',
+    'id': 3,
+    'url': 'https://1.bp.blogspot.com/-AMqzs5SYTzg/W6RGSdHsKcI/AAAAAAACVw0/yrsFd4lHLX8xKWM5GSP3qIhV722_mwdLwCLcBGAs/s1600/%25EC%2595%2584%25EC%259D%25B4%25EC%259C%25A0%2B169.jpg',
     'title': '두번째 사진'
   }
 ];
